12e28626aedd1a438a7f604189b8c3560269383d
rodriguembah13 <rodriguembah13@gmail.com>
2025-05-05 08:09:24 +0100

    dert

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::domain('sandbox.agensic.com')->group(function () {
         Route::match(["POST", "GET"], '/get_ajax_rate', [StaticController::class, 'getRateAjax'])->name('sandbox.get_ajax_rate');
     });
 });
-//Route::domain('secure.agensic.com')->group(function () {
+Route::domain('secure.agensic.com')->group(function () {
     Route::match(["POST", "GET"], '/', [SecuritySecureController::class, 'secureLogin'])->name('secure.login');
     Route::match(["POST", "GET"], '/register', [SecuritySecureController::class, 'secureRegister'])->name('secure.register');
     Route::match(["POST", "GET"], '/logout', [SecuritySecureController::class, 'logout'])->name('secure.logout');
@@ -75,7 +75,7 @@ Route::domain('sandbox.agensic.com')->group(function () {
         Route::match(["POST", "GET"], '/get_ajax_rate', [StaticSecureController::class, 'getRateAjax'])->name('secure.get_ajax_rate');
         Route::match(["POST", "GET"], '/make_deposit', [StaticSecureController::class, 'make_deposit'])->name('secure.make_deposit');
     });
-//});
+});
 
 Route::domain('manage.agensic.com')->group(function () {
     Route::match(["POST", "GET"], '/', [SecurityAdminController::class, 'adminLogin'])->name('admin.login');
